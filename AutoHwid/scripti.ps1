b8a1476b3ed74d23092abf320b649540e5725556
rekoriku <rerikuju@gmail.com>
2024-10-31 11:32:23 +0200

    Update scripti.ps1
    Error catching

--- AutoHwid/scripti.ps1
+++ AutoHwid/scripti.ps1
@@ -1,11 +1,30 @@
-$name = "AutopilotHWID"
-Get-WindowsAutopilotInfo -OutputFile "D:\$($name).csv" #-Append
-# If running in the console, wait for input before closing.
-if ($Host.Name -eq "ConsoleHost")
-{
-    Write-Host "Press any key to continue..."
-    $Host.UI.RawUI.FlushInputBuffer()   # Make sure buffered input doesn't "press a key" and skip the ReadKey().
-    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+function AutopilottiInformaatio {
+    $name = "AutopilotHWID"
+    Get-WindowsAutopilotInfo -OutputFile "D:\$($name).csv" #-Append
+    
+    # If running in the console, wait for input before closing.   
+    #Get-WindowsAutopilotInfo -OutputFile "D:\AutopilotHwid.csv" -Append
 }
 
-#Get-WindowsAutopilotInfo -OutputFile "D:\AutopilotHwid.csv" -Append
+function PressAnyKey($message) {
+    if ($Host.Name -eq "ConsoleHost") {
+        Write-Host "$($message)"
+        $Host.UI.RawUI.FlushInputBuffer()   # Make sure buffered input doesn't "press a key" and skip the ReadKey().
+        $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+    }
+}
+
+
+
+try {
+    AutopilottiInformaatio
+}
+catch {
+    Write-Host "Script failed trying again."
+    Start-Sleep -Seconds 1.0
+    AutopilottiInformaatio
+    <#Do this if a terminating exception happens#>
+}
+finally{
+    PressAnyKey('Press any key to close window...')
+}
