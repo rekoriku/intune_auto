08ee30300482eea491d2bb8a9a9aa5b0cda3d8c6
rekoriku <rerikuju@gmail.com>
2024-11-11 15:51:16 +0200

    Update update.bat
    check for folder

--- update.bat
+++ update.bat
@@ -1,5 +1,11 @@
 SET Dir=%~dp0
 SET HWID=%Dir%AutoHwid\
+
+if not exist %Dir%AutoHwid (
+    mkdir %Dir%AutoHwid
+    echo created %Dir%AutoHwid
+)
+
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %HWID%scripti.ps1 https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/AutoHwid/scripti.ps1";
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %HWID%AutoInstall.bat https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/AutoHwid/AutoInstall.bat;
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %Dir%AUTO.bat https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/AUTO.bat;
