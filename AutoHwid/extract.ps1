fc2d9e8d930033e7929e2e2679dbbb6e1e8351b9
rekoriku <rerikuju@gmail.com>
2024-11-25 11:36:49 +0200

    cleanup

--- AutoHwid/extract.ps1
+++ AutoHwid/extract.ps1
@@ -1,4 +1,3 @@
+$path = 'D:\SERIALIT.txt'
 $serials = Import-Csv -Path D:\AutopilotHWID.csv -Header 'Device Serial Number' | Select-Object -Skip 1
-#$P | Get-Member
-$serials | Where-Object -Property TopicTitle -Like '*' | Out-String -Stream | Where-Object { $_.Trim().Length -gt 0 } | Out-file -FilePath 'D:\SERIALIT.txt'
-#$serials | Where-Object { $_ } | Set-Content 'D:\SERIALIT.txt'
+$serials | Where-Object -Property TopicTitle -Like '*' | Out-String -Stream | Where-Object { $_.Trim().Length -gt 0 } | Out-file -FilePath $path
--- update.bat
+++ update.bat
@@ -8,6 +8,8 @@ if not exist %Dir%AutoHwid (
 
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %HWID%scripti.ps1 https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/AutoHwid/scripti.ps1";
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %HWID%AutoInstall.bat https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/AutoHwid/AutoInstall.bat;
+Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %HWID%extract.ps1 https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/AutoHwid/extract.ps1;
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %Dir%AUTO.bat https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/AUTO.bat;
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %Dir%aktivoi.bat https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/aktivoi.bat;
+Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %Dir%extract.bat https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/extract.bat;
 Call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "wget -O %Dir%update.bat https://raw.githubusercontent.com/rekoriku/intune_auto/refs/heads/main/update.bat;
